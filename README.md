b39a602eeeb7527ad72790b1091869eb26e384ff
Matt Collins <matt@strikesandgutters.com>
2017-02-06 16:43:32 +0000

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # sns-node-client
-A Node.JS client for the Simple Notification Service
+A Node.JS client for the [Simple Notification Service](https://github.com/ibm-cds-labs/simple-notification-service).
+
+Please refer to the Simple Notification Service readme file for the following:
+
+* [Sending Notifications](https://github.com/ibm-cds-labs/simple-notification-service#snssend-userquery-notificationdata-)
+* [Handling Events](https://github.com/ibm-cds-labs/simple-notification-service#events)
+
+## Connecting and Authentication
+There is a slight difference between the Node.JS client library and the Browser based client library, and that is how you connect and authenticate with the Simple Notification Service.
+
+As well as providing an API key, some `userData` and a `userQuery`, you will also need to provide a hostname. This hostname will be tied to the API key that you have supplied in exactly the same way as [via the browser](https://github.com/ibm-cds-labs/simple-notification-service#authentication).
+
+The way of providing this data is also slightly different:
+
+````js
+const SNSClient = require('sns-node-client')({
+  sns_host: '<YOUR_SNS_HOSTNAME>',
+  authentication: {
+    host: "<YOUR_AUTH_HOST>",
+    key: "<YOUR_API_KEY>"
+  },
+  userData: {
+    name: "Matt",
+    age: 32,
+    country: "USA",
+    user_type: "chat"
+  },
+  userQuery: {
+    country: "USA",
+    user_type: "chat"
+  }
+});
+````
---
+++ app.js
@@ -0,0 +1,139 @@
+"use strict";
+
+const socket = require('socket.io-client');
+const EventEmitter = require('events');
+const url = require('url');
+
+// socket.on('connect', function(){});
+// socket.on('event', function(data){});
+// socket.on('disconnect', function(){});
+
+const SNSClient = (opts = {}) => {
+
+  if (typeof opts.authentication !== "object" || opts.authentication === null) {
+    throw 'SNS: invalid authentication object provided';
+    return;
+  }
+
+  this.key = opts.authentication.key || "";
+  this.sns_host = opts.sns_host || ""
+  this.host = opts.authentication.host || "";
+  this.https = (this.host.match(/^https/) ? true : false)
+  this.userData = opts.userData || null
+  this.userQuery = opts.userQuery || null
+  this.socket = null;
+  this.events = new EventEmitter();
+  this.events.id = null;
+  this.events.connected = false;
+
+  /*
+   *  Validation
+   *  Check that we have the required information provided 
+   */
+ 
+
+  // check an sns_host host has been supplied
+  if (typeof this.sns_host != "string" || this.sns_host === "") {
+    throw 'SNS: an SNS hostname must be supplied';
+    return;
+  }
+
+  // check an authentication host has been supplied
+  if (typeof this.host != "string" || this.host === "") {
+    throw 'SNS: an authentication Host must be supplied';
+    return;
+  }
+
+  // check an API key has been supplied
+  if (typeof this.key != "string" || this.key === "") {
+    throw 'SNS: an API key must be supplied';
+    return;
+  }
+
+  //check that userData is passed in
+  if (typeof this.userData != "object") {
+    throw 'SNS: You must supply a valid Javascript object in the userData parameter';
+    return;
+  }
+
+  /*
+   *  Connect to Socket.IO server
+   */
+  this.socket = socket(this.sns_host); //our socket.io object
+
+  /*
+   *  Handle Socket.IO events
+   */
+
+  // provide descriptive data on connection
+  this.socket.on('connect', () => {
+    this.events.connected = true;
+    this.socket.emit('myData', { userData: this.userData, userQuery: this.userQuery, authentication: { key: this.key, hostname: this.host } })
+    this.events.id = "/#" + this.socket.id;
+    this.events.emit('connected');
+  });
+
+  // listen for authentication fail
+  this.socket.on('authenticationFail', (err) => {
+    this.socket.disconnect();
+    this.events.id = null;
+    this.events.connected = false;
+    throw new Error("SNS: Failed to authenticate");      
+  });
+
+  // listen for incoming messages
+  this.socket.on('notification', (data) => {
+    this.events.emit('notification', data);
+  });
+
+  // listen for incoming message event (used for front-end, undocumented)
+  this.socket.on('notificationPing', () => {
+    this.events.emit('notificationPing');
+  });
+
+  // listen for the currently connected users (that we care about, when we connect)
+  this.socket.on("currentUsers", (users) => {
+    this.events.emit('currentUsers', users);
+  })
+
+  // listen for newly connecting users (that we care about)
+  this.socket.on("connectedUser", (user) => {
+    this.events.emit('connectedUser', user);
+  })
+
+  // listen for newly connecting users (that we care about)
+  this.socket.on("disconnectedUser", (user) => {
+    this.events.emit('disconnectedUser', user);
+  });
+
+  /*
+   *  Websocket API methods
+   */
+
+  // websocket API send request
+  this.send = (query, data) => {
+
+    if (this.events.connected === false) {
+      throw 'SNS: not connected';
+      return;
+    }
+    
+    this.socket.emit('notification', { query: query, data: data })
+    
+  };
+
+  // disconnect from the server
+  this.disconnect = () => {
+
+    this.socket.disconnect();
+
+  };
+
+  this.events.send = this.send;
+  this.events.disconnect = this.disconnect;
+
+  return this.events;
+
+}
+
+module.exports = SNSClient;
